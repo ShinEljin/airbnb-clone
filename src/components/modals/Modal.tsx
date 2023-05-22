92d6cbbd0cfe2c3299c5326e1291c1adffda0f85
shineljin <psyruz18@gmail.com>
2023-05-22 22:04:44 +0800

    try fix

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -75,72 +75,76 @@ const Modal: React.FC<ModalProps> = ({
   }
 
   return (
-    <>
-      <Dialog
-        open={isOpen}
+    <Dialog
+      open={isOpen}
+      as="div"
+      onClose={handleClose}
+      className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70"
+    >
+      <Dialog.Panel
         as="div"
-        onClose={handleClose}
-        className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70"
+        className="relative w-full sm:w-[550px] my-6 mx-auto h-full sm:h-auto "
       >
-        <Dialog.Panel className="relative w-full sm:w-[550px] my-6 mx-auto h-full sm:h-auto ">
-          {/* CONTENT */}
-          <div
-            className={`duration-300 h-full 
+        {/* CONTENT */}
+        <div
+          className={`duration-300 h-full 
             ${showModal ? "translate-y-0" : "translate-y-full"} 
             ${showModal ? "op acity-100" : "opacity-0"}
             `}
-          >
-            <div className="h-full md:h-auto lg:h-auto  border-0 rounded-lg shadow-lg relative flex flex-col  w-full bg-white outline-none focus:outline-none">
-              {/* HEADER */}
-              <Dialog.Title className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
-                {userState === "" || !userState ? (
-                  <button
-                    className="p-1 border-0 hover:opacity-70 transition absolute left-9"
-                    onClick={handleClose}
-                  >
-                    <IoMdClose size={18} />
-                  </button>
-                ) : (
-                  <button
-                    className="p-1 border-0 hover:opacity-70 transition absolute left-9"
-                    onClick={handleBack}
-                  >
-                    <IoChevronBackOutline size={18} />
-                  </button>
-                )}
-                <div className="text-lg font-semibold">{title}</div>
-              </Dialog.Title>
+        >
+          <div className="h-full md:h-auto lg:h-auto  border-0 rounded-lg shadow-lg relative flex flex-col  w-full bg-white outline-none focus:outline-none">
+            {/* HEADER */}
+            <Dialog.Title
+              as="div"
+              className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]"
+            >
+              {userState === "" || !userState ? (
+                <button
+                  className="p-1 border-0 hover:opacity-70 transition absolute left-9"
+                  onClick={handleClose}
+                >
+                  <IoMdClose size={18} />
+                </button>
+              ) : (
+                <button
+                  className="p-1 border-0 hover:opacity-70 transition absolute left-9"
+                  onClick={handleBack}
+                >
+                  <IoChevronBackOutline size={18} />
+                </button>
+              )}
+              <div className="text-lg font-semibold">{title}</div>
+            </Dialog.Title>
 
-              <form onSubmit={onSubmit}>
-                {/* BODY */}
-                <div className="relative px-6 pt-6 flex-auto">{body}</div>
+            <form onSubmit={onSubmit}>
+              {/* BODY */}
+              <div className="relative px-6 pt-6 flex-auto">{body}</div>
 
-                {/* FOOTER */}
-                <div className="flex flex-col gap-2 px-6 pb-6 pt-4">
-                  <div className="flex items-center gap-4 w-full">
-                    {secondaryAction && secondaryActionLabel && (
-                      <Button
-                        disabled={disabled}
-                        label={secondaryActionLabel}
-                        onClick={handleSecondaryAction}
-                        outline
-                        type="button"
-                      />
-                    )}
+              {/* FOOTER */}
+              <div className="flex flex-col gap-2 px-6 pb-6 pt-4">
+                <div className="flex items-center gap-4 w-full">
+                  {secondaryAction && secondaryActionLabel && (
                     <Button
                       disabled={disabled}
-                      label={actionLabel}
-                      isLoading={isLoading}
+                      label={secondaryActionLabel}
+                      onClick={handleSecondaryAction}
+                      outline
+                      type="button"
                     />
-                  </div>
-                  {footer}
+                  )}
+                  <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    isLoading={isLoading}
+                  />
                 </div>
-              </form>
-            </div>
+                {footer}
+              </div>
+            </form>
           </div>
-        </Dialog.Panel>
-      </Dialog>
-    </>
+        </div>
+      </Dialog.Panel>
+    </Dialog>
   );
 };
 export default Modal;

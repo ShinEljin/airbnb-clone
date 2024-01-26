9378c58a77ba27a8063072f369300906b2f54829
raphaelenciso <psyruz18@gmail.com>
2024-01-26 16:26:35 +0800

    typescript error

--- src/utils/getCurrentUser.ts
+++ src/utils/getCurrentUser.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
+import { authOptions } from "@/lib/authOptions";
 
 export async function getSession() {
   return await getServerSession(authOptions);
